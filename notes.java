c772a2f2142349849b750325947c8d34be4dde0c
Karin Touchie <ktouchie@gmail.com>
2016-01-05 11:02:52 +0100

    Declare array of grades /20

--- notes.java
+++ notes.java
@@ -3,8 +3,10 @@ class notes {
     
     public static void main(String[] args) {
         
-        
+        int[][] notes = new int{{19,18,12,7,16},{3,7,5,9,1},{20,16,14,20,11},{10,9,8,7,6},{14,12,3,19,5},{1,2,3,0,4}};
         
     }
+    
+    
     
 }
